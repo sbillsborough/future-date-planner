090516319a07b4791b278e7c5404b3bc46b02259
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-12-10 16:33:12 +0000

    current day displays with moment.js and time-block class added to a div for schedule items

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// current day displayed
+// table or rows that have the hour, text area and save button
+// save button links to corresponding text area
+// saves to local storage
+
+var currentDay = moment().format("dddd MMMM Do");
+$("#currentDay").text(currentDay);
