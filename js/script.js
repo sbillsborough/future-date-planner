c6ecb3de49fac667f193054c5f2dd432b92bbe48
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-12-10 17:12:29 +0000

    added saveBtn class to td element for styling

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // table or rows that have the hour, text area and save button
 // save button links to corresponding text area
 // saves to local storage
+// onclick/active/current state add past, present, future css class
+// popup message on save, hidden class that temporarily removes when save button is pressed (set interval??)
 
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
